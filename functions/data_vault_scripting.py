f5e217edf286300621a21c03310633839d73c5ef
skinnypigeon <euan.blackledge@soprasteria.com>
2020-10-08 21:35:28 +0100

    woohoo, hubs worked nicely, so hopefully the rest should too

--- functions/data_vault_scripting.py
+++ functions/data_vault_scripting.py
@@ -1,34 +1,7 @@
-hubs = {
-    'hub_time': {
-        'einri': {'data_type': 'varchar'}, 
-        'falnr': {'data_type': 'varchar'}, 
-        'patnr': {'data_type': 'integer'}, 
-        'pernr': {'data_type': 'varchar'}
-    }, 
-    'hub_person': {
-        'id': {'data_type': 'integer'}
-    }, 
-    'hub_object': {
-        'einri': {'data_type': 'varchar'}, 
-        'patnr': {'data_type': 'integer'}, 
-        'falnr': {'data_type': 'varchar'}, 
-        'pernr': {'data_type': 'varchar'}
-    }, 
-    'hub_location': {
-        'patnr': {'data_type': 'integer'}
-    }, 
-    'hub_event': {
-        'einri': {'data_type': 'varchar'}, 
-        'falnr': {'data_type': 'varchar'}, 
-        'patnr': {'data_type': 'integer'}, 
-        'pernr': {'data_type': 'varchar'}
-    }
-}
-
 import os
 from pathlib import Path
 
-project_folder = os.path.expanduser('~/code/data_vault_generator/')
+project_folder = os.path.expanduser('~/code/data_vault_maker_backend/')
 
 def data_vault_boilerplate(output_file, schema, database):
     class_prefix = schema.upper()
@@ -38,13 +11,14 @@ def data_vault_boilerplate(output_file, schema, database):
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
-from sqlalchemy import Column, Integer, String, Numeric, DateTime, TimeStamp, Text, ForeignKey, create_engine
+from sqlalchemy import Column, Integer, String, Numeric, DateTime, Text, ForeignKey, create_engine
+from sqlalchemy.types import TIMESTAMP
 
 import os
 from dotenv import load_dotenv
 from pathlib import Path
 
-project_folder = os.path.expanduser('~/code/data_vault_generator/')
+project_folder = os.path.expanduser('~/code/data_vault_maker_backend/')
 load_dotenv(os.path.join(project_folder, '.env'))
 PASSWORD = os.getenv('PASSWORD')
 
@@ -64,7 +38,7 @@ column_types = {{
     'numeric': Numeric,
     'datetime': DateTime,
     'text': Text,
-    'timestamp without time zone': 'TimeStamp'
+    'timestamp without time zone': TIMESTAMP
 }}
 
 # HUBS
@@ -74,7 +48,7 @@ column_types = {{
     f.close()
 
 
-def hubs_scripting(hubs, schema):
+def hubs_scripting(output_file, hubs, schema):
     class_prefix = schema.upper()
     for keys, values in hubs.items():
         class_variable = keys[4:]
@@ -92,16 +66,8 @@ for key in primary_keys:
 {class_variable} = type('{class_name}',(Base,),{hub_name})
         """.format(hub_name=keys, schema=schema, values=values, class_variable=class_variable, class_name=class_name)
         print(hub_text)
-hubs_scripting(hubs, "public")
+        f = open('./output_data_vault_files/{output_file}.py'.format(schema=schema, output_file=output_file), 'a+')
+        f.write(hub_text)
+        f.close()
 
-# data_vault_boilerplate("test", "public", "vault_bot_out")
-
-# hub_time={'__tablename__': 'hub_time',
-# '__table_args__':{'schema':'fcrb'},
-# 'id': Column(column_types['integer'], primary_key=True)}
-
-# keys = [{'einri': Column(String)},{'patnr': Column(Integer)},{'falnr': Column(String)},{'pernr': Column(String)},{'orgid': Column(String)},{'vppid': Column(Integer)}]
-# for key in keys:
-#     hub_time.update(key)
-
-# time = type('FCRB_Hub_Time',(Base,),hub_time)
+# hubs_scripting("test", hubs, "public")
--- output_data_vault_files/test.py
+++ output_data_vault_files/test.py
@@ -4,8 +4,8 @@
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
-from sqlalchemy import Column, Integer, String, Numeric, DateTime, TimeStamp, Text, ForeignKey, create_engine
-
+from sqlalchemy import Column, Integer, String, Numeric, DateTime, Text, ForeignKey, create_engine
+from sqlalchemy.types import TIMESTAMP
 import os
 from dotenv import load_dotenv
 from pathlib import Path
@@ -30,8 +30,66 @@ column_types = {
     'numeric': Numeric,
     'datetime': DateTime,
     'text': Text,
-    'timestamp without time zone': 'TimeStamp'
+    'timestamp without time zone': TIMESTAMP
 }
 
 # HUBS
     
+hub_time={'__tablename__': 'hub_time',
+'__table_args__':{'schema':'public'},
+'id': Column(column_types['integer'], primary_key=True)}
+
+primary_keys = []
+for keys, values in {'einri': {'data_type': 'varchar'}, 'falnr': {'data_type': 'varchar'}, 'patnr': {'data_type': 'integer'}, 'pernr': {'data_type': 'varchar'}}.items():
+    primary_keys.append({keys: Column(column_types[values['data_type']])})
+for key in primary_keys:
+    hub_time.update(key)
+
+time = type('PUBLIC_Hub_Time',(Base,),hub_time)
+        
+hub_person={'__tablename__': 'hub_person',
+'__table_args__':{'schema':'public'},
+'id': Column(column_types['integer'], primary_key=True)}
+
+primary_keys = []
+for keys, values in {'pid': {'data_type': 'integer'}}.items():
+    primary_keys.append({keys: Column(column_types[values['data_type']])})
+for key in primary_keys:
+    hub_person.update(key)
+
+person = type('PUBLIC_Hub_Person',(Base,),hub_person)
+        
+hub_object={'__tablename__': 'hub_object',
+'__table_args__':{'schema':'public'},
+'id': Column(column_types['integer'], primary_key=True)}
+primary_keys = []
+for keys, values in {'einri': {'data_type': 'varchar'}, 'patnr': {'data_type': 'integer'}, 'falnr': {'data_type': 'varchar'}, 'pernr': {'data_type': 'varchar'}}.items():
+    primary_keys.append({keys: Column(column_types[values['data_type']])})
+for key in primary_keys:
+    hub_object.update(key)
+
+object = type('PUBLIC_Hub_Object',(Base,),hub_object)
+        
+hub_location={'__tablename__': 'hub_location',
+'__table_args__':{'schema':'public'},
+'id': Column(column_types['integer'], primary_key=True)}
+primary_keys = []
+for keys, values in {'patnr': {'data_type': 'integer'}}.items():
+    primary_keys.append({keys: Column(column_types[values['data_type']])})
+for key in primary_keys:
+    hub_location.update(key)
+
+location = type('PUBLIC_Hub_Location',(Base,),hub_location)
+        
+hub_event={'__tablename__': 'hub_event',
+'__table_args__':{'schema':'public'},
+'id': Column(column_types['integer'], primary_key=True)}
+primary_keys = []
+for keys, values in {'einri': {'data_type': 'varchar'}, 'falnr': {'data_type': 'varchar'}, 'patnr': {'data_type': 'integer'}, 'pernr': {'data_type': 'varchar'}}.items():
+    primary_keys.append({keys: Column(column_types[values['data_type']])})
+for key in primary_keys:
+    hub_event.update(key)
+
+event = type('PUBLIC_Hub_Event',(Base,),hub_event)
+        
+Base.metadata.create_all(engine)
