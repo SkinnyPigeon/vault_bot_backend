d99a6c034af54fc7686e28450f2ff39f20ae455d
skinnypigeon <euan.blackledge@soprasteria.com>
2020-10-08 21:24:09 +0100

    need to test this out to see

--- functions/data_vault_scripting.py
+++ functions/data_vault_scripting.py
@@ -30,8 +30,6 @@ from pathlib import Path
 
 project_folder = os.path.expanduser('~/code/data_vault_generator/')
 
-
-
 def data_vault_boilerplate(output_file, schema, database):
     class_prefix = schema.upper()
     setup = """
@@ -76,9 +74,34 @@ column_types = {{
     f.close()
 
 
-def hubs_scripting(hubs):
-    print(hubs)
+def hubs_scripting(hubs, schema):
+    class_prefix = schema.upper()
+    for keys, values in hubs.items():
+        class_variable = keys[4:]
+        class_name = "{class_prefix}_".format(class_prefix=class_prefix) + keys.replace('_', ' ').title().replace(' ', '_')
+        hub_text = """
+{hub_name}={{'__tablename__': '{hub_name}',
+'__table_args__':{{'schema':'{schema}'}},
+'id': Column(column_types['integer'], primary_key=True)}}
+primary_keys = []
+for keys, values in {values}.items():
+    primary_keys.append({{keys: Column(column_types[values['data_type']])}})
+for key in primary_keys:
+    {hub_name}.update(key)
 
-hubs_scripting(hubs)
+{class_variable} = type('{class_name}',(Base,),{hub_name})
+        """.format(hub_name=keys, schema=schema, values=values, class_variable=class_variable, class_name=class_name)
+        print(hub_text)
+hubs_scripting(hubs, "public")
 
-data_vault_boilerplate("test", "public", "vault_bot_out")
+# data_vault_boilerplate("test", "public", "vault_bot_out")
+
+# hub_time={'__tablename__': 'hub_time',
+# '__table_args__':{'schema':'fcrb'},
+# 'id': Column(column_types['integer'], primary_key=True)}
+
+# keys = [{'einri': Column(String)},{'patnr': Column(Integer)},{'falnr': Column(String)},{'pernr': Column(String)},{'orgid': Column(String)},{'vppid': Column(Integer)}]
+# for key in keys:
+#     hub_time.update(key)
+
+# time = type('FCRB_Hub_Time',(Base,),hub_time)
